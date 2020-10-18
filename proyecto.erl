c05a3f948e77b82fd9a0f43956e6d77442e9f1a0
Josue Cespedes <josue.cespedes19@gmail.com>
2020-10-18 01:11:05 -0600

    Documentacion y print del elite actual

--- proyecto.erl
+++ proyecto.erl
@@ -1,7 +1,6 @@
 -module(proyecto).
 -define(MUTATION, 0.05).
--define(MAX_CRUCES, 800).
--import(lists,[min/1]).
+-define(MAX_CRUCES, 1000).
 -export([poblacion/1, aptitud/3, cruces/4, geneticosNReinas/1, min_pos/3, pos_elemento/2, ejecutar/1]).
 
 %% N = Reinas
@@ -60,18 +59,25 @@ aptitud([H|T], N, C, R) -> aptitud(T, N, C-1, [suma_colisiones(H,N)|R] ).
 %% El target para este ejercicio es que las reinas no se ataquen, por lo que cada individuo calcula cuantas colisiones 
 %% provocan sus genes(elementos de la lista)
 
+%% Dominio: Lista de población, Individuo elite, N reinas, Cantidad de población
+%% Codominio: una lista con la nueva generación de individuos
 cruces(L, E, N, C) -> cruce(L, N, C-1, [E]).
 cruce(_, _, 0, R) -> lists:reverse(R);
 cruce(L, N, C, R) ->  cruce(L, N, C-1, [cruce_aux(L, N, N*4)|R]).
 
+%% Dominio: Una lista de población, Reinas y cantidad de población
+%% Codominio: Una lista con genes de un nuevo individuo que forma parte de la nueva población
 cruce_aux(L, N, C) -> X1 = rand:uniform(C),
                       X2 = rand:uniform(C), 
                       padres(L, N, X1, X2).
 
+%% Dominio: Lista de población, Número de reinas, y dos padres(individuos) aleatorios
+%% Codominio: Una lista con genes de un nuevo individuo que forma parte de la nueva población
 padres(L, N, X1, X2) -> P1 = lists:nth(X1, L), 
                         P2 = lists:nth(X2, L),
                         reproduccion(P1, P2, N).
-
+%% Dominio: Dos padres aleatorio y número de reinas
+%% Codominio: Una lista con genes de un nuevo individuo que forma parte de la nueva población evaluando la posiblidad de mutación
 reproduccion(P1, P2, N) -> L = split(P1, trunc(N/2)), 
                            Hijo = reproducirse(P2, lists:reverse(L)),
                            Rand = rand:uniform(),
@@ -79,12 +85,15 @@ reproduccion(P1, P2, N) -> L = split(P1, trunc(N/2)),
                            true  -> mutacion(Hijo, N);
                            false ->  Hijo end.
 
+%% Dominio: Una lista de genes de un padre y otra lista con genes del segundo padre
+%% Codominio: Un nuevo individuo con los genes de ambos padres
 reproducirse([], R) -> lists:reverse(R);
 reproducirse([H|T], R) -> case lists:member(H, R) of 
                           true -> reproducirse(T, R); 
                           false -> reproducirse(T, [H|R]) end.
 
-% lista y posicion a split
+%% Dominio: Una lista L  y un número N que indica posición a split
+%% Codominio: Una sublista desde  hasta N
 split(L, N) -> split(L, N, 0, [], []).
 split([], _, _, L1, _)-> lists:reverse(L1);
 split([H|T], N, C, L1, []) when N > C -> split(T, N, C+1, [H|L1], []);
@@ -98,13 +107,14 @@ split([H|T], N, C, L1, L2)-> split(T,N, C+1, L1, [H|L2]).
 %% Mutaciones
 
 %% L = Población, N = Reinas, M = Mutaciones, C = Cantidad de población
+%% Dominio: un individuo seleccionado, número de reinas
+%% Codominio: Un inviduo que presenta mutación en sus genes
 mutacion(Picked, N) ->  Gen1 = rand:uniform(N),
                         Gen2 = rand:uniform(N),
-                        erlang:display('mutation found'),
-                        erlang:display(Picked),
                         mutar(lists:nth(Gen1, Picked), lists:nth(Gen2, Picked), Picked).
  
-
+%% Dominio: Genes seleccionados a mutar y lista actual de genes
+%% Codominio: Lista con los genes mutados
 mutar(Gen1, Gen1, List) -> List;
 mutar(Gen1, Gen2, List) -> {P1, P2} = {min(Gen1,Gen2), max(Gen1,Gen2)},
                            {L1, [Elem1 | T1]} = lists:split(P1-1, List),
@@ -113,22 +123,34 @@ mutar(Gen1, Gen2, List) -> {P1, P2} = {min(Gen1,Gen2), max(Gen1,Gen2)},
 
 
 %% mostrar solución 
-mostrarSolucion(_, _, 0, _, R) -> lists:reverse(R);
+%% Dominio: Una lista de genes, número de reinas, cantidad de población 
+%% Codominio: Impresión en pantalla del individuo que brinda solución
+mostrarSolucion(_, _, 0, _, R) -> io:fwrite("Solución:~n", []), lists:reverse(R);
 mostrarSolucion(L, N, S, C, R) -> Pos = pos_elemento(S, L),
                                   QL = generate(Pos, N),
                                   mostrarSolucion(L, N, S-1, C, [QL|R]).
 
+%% Dominio: Una posición de la lista de genes, Número de reinas
+%% Codominio: Una lista que representa una fila del tablero de ajedrez
 generate(Pos, N) -> generate(Pos, N, []).
 generate(_, 0, R)-> R;
 generate(Pos, N, R) -> case Pos == N of
                        true -> generate(Pos, N-1, ['♕'|R]);
                        false -> generate(Pos, N-1, ['■'|R]) end.
-
-geneticosNReinas(N) -> 
+%% función principal
+%% Dominio: Un n natural mayor que 3
+%% Codominio: Una solución al problema de NReinas
+geneticosNReinas(N) when N > 3 -> 
                     Poblacion = poblacion(N),
                     calcular(?MAX_CRUCES, Poblacion, N, N*4).
-
-
+%%Dominio: Elemento con mejor aptitud, Cantidad de cruces efectuados
+%% Codominio: Impresión por pantalla de elite actual junto con el número de cruce
+eliteActual(Elite, Cruces) ->   io:fwrite("Cruce: ~p~n", [?MAX_CRUCES -Cruces]),
+                                io:fwrite("Élite actual: ", []),
+                                erlang:display(Elite),
+                                io:fwrite("~n", []).
+%% Dominio: cantidad de cruces, poblacióm, Reinas y cantidad de población
+%% Codominio: cantidad de cruces necesarios para otorgar una respuesta en dado de que se encuentre
 calcular(0, _, _, _) -> [];
 calcular(Cruces, Poblacion, N, C) -> Aptitudes = aptitud(Poblacion, N, C),
                                      Min = trunc(lists:min(Aptitudes)),      
@@ -136,17 +158,21 @@ calcular(Cruces, Poblacion, N, C) -> Aptitudes = aptitud(Poblacion, N, C),
                                      Elite = lists:nth(Pos, Poblacion),
                                      case Min == 0 of
                                      true -> mostrarSolucion(Elite, N, N, Cruces, []);
-                                     false -> N_Poblacion = cruces(Poblacion, Elite, N, C),
+                                     false -> eliteActual(Elite, Cruces),
+                                              N_Poblacion = cruces(Poblacion, Elite, N, C),
                                               calcular(Cruces-1, N_Poblacion, N, C) end.
-
+%% Dominio: Lista, Cabeza, Posición
+%% Codominio: Posición actual de un elemento
 min_pos([H|_], H, R)-> R;
 min_pos([_|T], N, R)->min_pos(T, N, R+1).
 
+%% Dominio: Elemento, Lista
+%% Codominio: posición de un elemento
 pos_elemento(N, L) -> pos_elemento(N, 1, L).
 pos_elemento(H, N, [H|_]) -> N;
 pos_elemento(N, P, [_|R]) -> pos_elemento(N, P+1, R).
-
-
-ejecutar(N) -> ejecutar(N, 10).
-ejecutar(_, 0) -> 0;
-ejecutar(N, C) -> geneticosNReinas(N), ejecutar(N, C-1).
+ 
+%% PRUEBAS PARA RESULTADOS
+ejecutar(N) -> ejecutar(N, 1).
+ejecutar(_, 0) -> 0; 
+ejecutar(N, C) -> RP = geneticosNReinas(N), case RP == [] of true-> false; false-> ejecutar(N, C-1) end. 
